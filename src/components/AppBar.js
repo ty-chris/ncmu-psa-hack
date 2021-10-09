f1004dea3b9ac63465fd93956679cf640ba889d7
isaactyj <77622789+isaactyj@users.noreply.github.com>
2021-10-09 11:36:37 +0800

    Update AppBar.js

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,12 +6,14 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import icon from '../PSAI_Logo_Padded.png'
 
 export default function ButtonAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
+                <img src={logo} className={classes.logo}/>
                     <IconButton
                         size="large"
                         edge="start"
