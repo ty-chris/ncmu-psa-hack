018ccc859fabceb443f691f9cdfb9ecfd341de6e
Chris Ng <tingyou@hotmail.co.uk>
2021-10-09 11:30:50 +0800

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import ButtonAppBar from './components/AppBar';
+import LandingPage from './components/layout/LandingPage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <ButtonAppBar />
+                <Switch>
+                    <Route path="/" exact component={LandingPage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/layout/LandingPage.js
@@ -0,0 +1,3 @@
+export default function ButtonAppBar() {
+    return <div>Hello</div>;
+}
