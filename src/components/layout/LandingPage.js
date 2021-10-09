a52769832ba04bc62f959895542d970edf2bbc2f
Chris Ng <tingyou@hotmail.co.uk>
2021-10-09 12:04:46 +0800

    add form

--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -1,3 +1,17 @@
-export default function ButtonAppBar() {
-    return <div>Hello</div>;
+export default function LandingPage() {
+    return (
+        <div>
+            <iframe
+                title="form"
+                src="https://docs.google.com/forms/d/e/1FAIpQLSc1cOndSu7q1fwPTDH_dlm7fcBHtq5HfAigfNY826Z-NFT2BA/viewform?embedded=true"
+                width="640"
+                height="1000"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+            >
+                Loading…
+            </iframe>
+        </div>
+    );
 }
