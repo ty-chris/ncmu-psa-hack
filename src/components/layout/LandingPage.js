0753a4d019e07dd5576e0d9e9c453e90671afe80
Chris Ng <tingyou@hotmail.co.uk>
2021-10-09 12:08:23 +0800

    add title

--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -1,6 +1,12 @@
+import { Typography } from '@mui/material';
+
 export default function LandingPage() {
     return (
         <div>
+            <Typography style={{ paddingTop: '20px' }} variant="h1">
+                {' '}
+                PSA Cargo Management System{' '}
+            </Typography>
             <iframe
                 title="form"
                 src="https://docs.google.com/forms/d/e/1FAIpQLSc1cOndSu7q1fwPTDH_dlm7fcBHtq5HfAigfNY826Z-NFT2BA/viewform?embedded=true"
